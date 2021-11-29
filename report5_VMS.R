9313d485b838422db59ddc3d8f23dbfa07b9fa5e
Colin Millar <colin.millar@ices.dk>
2021-11-29 01:22:44 +0100

    Update report5_VMS.R

--- report5_VMS.R
+++ report5_VMS.R
@@ -8,11 +8,7 @@ library(sf)
 mkdir("report")
 
 # file name utilitiy
-file_name <- function(name, ext = "") {
-  name <- gsub(" ", "_", name)
-  if (nzchar(ext)) ext <- paste0(".", ext)
-  paste0("2021_ONA_FO_", name, ext)
-}
+source("bootstrap/utilities.r")
 
 
 ecoregion <-
